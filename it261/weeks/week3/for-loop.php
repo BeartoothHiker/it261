4b4b3226bc6c421f06e1e8af2477f1a6969d6e29
Jeremy R <72467083+BeartoothHiker@users.noreply.github.com>
2022-02-11 21:06:50 -0800

    Fix validation errors

--- it261/weeks/week3/for-loop.php
+++ it261/weeks/week3/for-loop.php
@@ -15,7 +15,7 @@ $farenheit = $celcius * 9/5 + 32;
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>PHP for-loop</title>
   <style>
 * {
     padding: 0;
