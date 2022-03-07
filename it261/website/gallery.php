98d9588fa9efc81974d14c6f205bfa0004ddfc77
Jeremy R <72467083+BeartoothHiker@users.noreply.github.com>
2022-03-06 18:52:37 -0800

    Minor cleanup of gallery page

--- it261/website/gallery.php
+++ it261/website/gallery.php
@@ -6,10 +6,9 @@ include('includes/header.php');
   <div id="wrapper-buffer"></div>
 
   <div id="wrapper">
-    <!-- <main> -->
-      <h2><?php echo $headline; ?></h2>
-      <p>A Seattle Public library card offers a number of benefits you might not expect.  You can access services including online publications and traing services. Additionally it includes free music and vido streaming services, and busuiness and financial research resources.  Your Library helps you get free admission to participating Seattle museums. Enjoy exhibits on aviation, nature, science, and industry—all for free! Have a favorite museum you’d like to revisit? You can reserve one pass to a museum once every 30 days from the date of your visit.</p>
-      <table>
+    <h2><?php echo $headline; ?></h2>
+    <p>Your Seattle Public Library card offers a number of benefits you might not expect.  You can access services including online publications and traing services. Additionally it includes free music and vido streaming services, and busuiness and financial research resources.  Your Library helps you get free admission to participating Seattle museums. Enjoy exhibits on aviation, nature, science, and industry—all for free! Have a favorite museum you’d like to revisit? You can reserve one pass to a museum once every 30 days from the date of your visit.</p>
+    <table>
 
 <?php
 foreach($services as $name => $list) :
@@ -33,13 +32,6 @@ foreach($services as $name => $list) :
 
     </table>
 
-    <!-- </main> -->
-    <!-- 
-    <aside>
-      <h3>Additional Info</h3>
-      <p>Information about the gallery</p>
-    </aside>
-    -->
   </div> <!-- end wrapper -->
 
   <div id="footer-clear"></div>
