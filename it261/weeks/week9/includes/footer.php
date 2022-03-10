bc58eb024b756c8466a7a0d98ff32b4e67d695b0
Jeremy R <72467083+BeartoothHiker@users.noreply.github.com>
2022-03-10 11:15:24 -0800

    Week 9 exercises & transfer some stuff to week 8

---
+++ it261/weeks/week9/includes/footer.php
@@ -0,0 +1,21 @@
+
+  </div>   <!-- end wrapper -->
+  <footer>
+    <div class="inner">
+      <ul>
+        <li>Copyright <?= date('Y') ?></li>
+        <li>All Rights Reserved</li>
+        <li><a href="../../index.php">Web Design By CodeWithJeremy</a></li>
+        <li><a id="html-validator" href="#">HTML Validation</a></li>
+        <li><a id="css-validator" href="#">CSS Validation</a></li>
+        <li><a href="https://github.com/BeartoothHiker/it261">GitHub</a></li>
+      </ul>
+    </div>
+  </footer>
+
+  <script>
+    document.getElementById("html-validator").setAttribute("href", "https://validator.w3.org/nu/?doc=" + location.href);
+    document.getElementById("css-validator").setAttribute("href", "https://jigsaw.w3.org/css-validator/validator?uri=" + location.href);
+  </script>
+</body>
+</html>
